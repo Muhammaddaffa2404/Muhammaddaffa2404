69d3fd8b5af00790f0f92419826bc36af4ac77bc
Muhammaddaffa2404 <113499765+Muhammaddaffa2404@users.noreply.github.com>
2022-09-14 15:11:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+    <link real="styleshet" herf="style.css" type="text/css">
+</head>
+<body>
+
+<!-- <h2>Login Form </h2> -->
+<form action="tampil.php" method="POST">
+    <h2>Form Login </h2>
+    <div class="imgcontainer">
+        <img src="icon.png" alt="Avatar" class="avatar">
+    </div>
+  
+    <div class="container
+        <label><Username</b></label>
+         <input  typw="text"  placeholder="Masukan Username"
+name="username"  required>
+       
+       <label><b>Password</b></label>
+       <input  type="password"   placeholder="Masukan Password"
+name="email" required>
+
+      <button  type="sumbit">Masuk</button>
+    <input tyep="checkbox" checked"><span>Ingat Saya</span>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+        <button type="button"  class="cancelbtn">Batal</button>
+        <span  class="psw">Lupa <a herf="#">password?</a></span>
+      </div>
+</form>
+
+</body>
+</html>
